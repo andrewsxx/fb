13acdfbfb958a5306f948435728ff9a313d17043
Andrés Peña <ayn_nian@hotmail.com>
2014-06-13 21:17:09 -0500

    facebook root

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <html>
     <head>
         <link rel="stylesheet" href="css/bootstrap.css">
+        <script type="text/javascript" src="js/jquery-1.11.0.js"></script>
         <script type="text/javascript" src="js/bootstrap.min.js"></script>
         <script type="text/javascript" src="js/main.js"></script>
     </head>
     <body>
+        <div id="fb-root"></div>
         <div class="container" style="margin-top: 20px;">
             <div class="row">
                 <a href="#" class="btn btn-default">connect by js</a>
--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+$(document).ready(function(){
+   var appId = '159162207495855'; 
+   var secret = '8bb887a4b36f9c841f2c19803e2e0975';
+   
+   window.fbAsyncInit = function() {
+        FB.init({appId: appId, status: true, cookie: true,
+                 xfbml: true, oauth: true});
+//        for(var item = 0; item < this.onLoadMethods.length; item++){
+//            this.onLoadMethods[item]();
+//        }
+    };
+    (function() {
+        var e = document.createElement('script');
+        e.async = true;
+        e.src = document.location.protocol +
+        '//connect.facebook.net/en_US/all.js';
+        document.getElementById('fb-root').appendChild(e);
+    }());
+});
