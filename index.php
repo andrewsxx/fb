c0835d1afae4624aff02f8f2f60fa2e3537068d5
Andrés Peña <ayn_nian@hotmail.com>
2014-06-13 19:53:14 -0500

    step 1

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+    </head>
+    <body>
+        <div class="container" style="margin-top: 20px;">
+            <div class="row">
+                <a href="#" class="btn btn-default">connect by js</a>
+            </div>
+        </div>
+    </body>
+</html>
